04658a847e8c806f6570a7d1c00e53ed6fad09e6
ptagliavia <79185768+ptagliavia@users.noreply.github.com>
2023-03-05 16:53:00 -0800

    Changed Hello World

--- myfirst.js
+++ myfirst.js
@@ -1,18 +1,9 @@
-const http = require('http'); // Loads the http module
-
-http.createServer((request, response) => {
-
-    // 1. Tell the browser everything is OK (Status code 200), and the data is in plain text
-    response.writeHead(200, {
-        'Content-Type': 'text/plain'
-    });
-
-    // 2. Write the announced text to the body of the page
-    response.write('Hello, World!\n');
-
-    // 3. Tell the server that all of the response headers and body have been sent
-    response.end();
-
-}).listen(1337); // 4. Tells the server what port to be on
-
-//testing to see if I can pull
+const http = require("http");
+const hostname = "localhost";
+const port = 3000;
+const server = http.createServer((req, res) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.end("<html><body><h1>Hello, World!</h1></body></html>");
+})
+server.listen(port, hostname);
