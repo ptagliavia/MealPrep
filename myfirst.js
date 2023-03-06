c7eb62f6a7a3b305c4726eb4c9aec6a881637ce8
ptagliavia <79185768+ptagliavia@users.noreply.github.com>
2023-03-05 17:18:31 -0800

    Changed Hello World

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,10 @@
-const http = require("http");
-const hostname = "localhost";
-const port = 3000;
-const server = http.createServer((req, res) => {
-    console.log(req.headers);
-    res.statusCode = 200;
-    res.end("<html><body><h1>Hello, World!</h1></body></html>");
-})
-server.listen(port, hostname);
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('Hello World from GCE!');
+});
+
+app.listen(3000, function () {
+    console.log('Example app listening on port 3000!');
+});
