49d4c936adbaceb8b76de2aec6c3eef56a1f6521
ptagliavia <79185768+ptagliavia@users.noreply.github.com>
2023-03-06 18:23:24 -0800

    Using alternate display text and syntax for express routing/listening app

--- myfirst.js
+++ myfirst.js
@@ -1,9 +1,15 @@
 const express = require('express');
-var app = express();
+const app = express();
 
-app.get('/', function (req, res) {
-    res.send('Hello World from GCE!');
+app.get('/', (reg,res) => {
+    res.send({hi: 'there'})
 });
+
+
+//alt syntax using bodied function
+/*app.get('/', function (req, res) {
+    res.send('Hello World from GCE!');
+});*/
 
 app.listen(3000, function () {
     console.log('Example app listening on port 3000, now improved!');
